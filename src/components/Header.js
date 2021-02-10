148e2a1ae6a20422c74404bba280bf83950b66a1
mPeltzerNovi <m.peltzer@novi-education.nl>
2021-02-10 14:01:35 +0100

    Added basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,13 @@ import { useHistory } from 'react-router-dom';
 import Button from './Button';
 import {AuthContext, useAuthState} from "../context/AuthContext";
 
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import { Avatar } from "@material-ui/core";
 
 
 function Header() {
@@ -21,19 +28,58 @@ function Header() {
 
     return (
         <header>
-            <div>
-                <Button>
+            <div className="header_left">
+                <img src="https://upload.wikimedia.org/wikipedia/en/8/8c/Facebook_Home_logo_old.svg" alt=""/>
+                <div className="header_input">
+                    <SearchIcon />
+                    <input placeholder="Search Facebook" type="text"/>
+                </div>
+            </div>
+
+
+
+            <div className="header_center">
+                <div className="header_option header_option--active">
+                    <HomeIcon fontSize="large" />
+                </div>
+                <div className="header_option">
+                    <FlagIcon fontSize="large" />
+                </div>
+                <div className="header_option">
+                    <SubscriptionsOutlinedIcon fontSize="large" />
+                </div>
+                <div className="header_option">
+                    <StorefrontOutlinedIcon fontSize="large" />
+                </div>
+                <div className="header_option">
+                    <SupervisedUserCircleIcon fontSize="large" />
+                </div>
+            </div>
+
+            <div className="header-right">
+
+
+
+
+                {/*<Button>
                     Hallo daar!
-                </Button>
+                </Button>*/}
 
 
                 {isAuthenticated ? (
-                    <button
-                        type="button"
-                        onClick={() => logout()}
-                    >
-                        Log uit
-                    </button>
+                    <>
+                        <div className="header_info">
+                            <Avatar />
+                            <h4>Iris Dekker</h4>
+                                <button
+                                    type="button"
+                                    onClick={() => logout()}
+                                >
+                                    Log uit
+                                </button>
+                        </div>
+                    </>
+
                 ) : (
                     <>
                         <button
@@ -48,9 +94,13 @@ function Header() {
                         >
                             Registreren
                         </button>
+
                     </>
                 )}
             </div>
+
+
+
         </header>
     );
 };
